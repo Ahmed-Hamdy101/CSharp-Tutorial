75aedae40da92485a85636aef36749fdd2e67ae5
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-03-31 06:21:20 +0200

    Switch case

--- Ex5_csharp-switchcase/switch case.cs
+++ Ex5_csharp-switchcase/switch case.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +9,24 @@ namespace CSharp.g.Cond_Switchcase
     class Program
     {
         static void Main(string[] args)
-        {
+        {  // define array with inputs 
             string []spec = new string[5];
             spec[0] = "@";
             spec[1] = ".";
             spec[2] = "#";
             spec[3] = "!";
             spec[4] = "%";
-
+             // output message 
             Console.WriteLine("Enter Your Email :") ;
+            // user input
             var  email = Console.ReadLine();
 
             switch ( email )
-            {
+            {   // valid the email
                 case "Someone.33@gmail.com":
                     Console.WriteLine("Corrected!!");
                     break;
+                    // if is not then is invalid
                 default:
                     Console.WriteLine("incorrected!!");
                     break;
