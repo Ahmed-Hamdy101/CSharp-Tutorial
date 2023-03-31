07d2e05f95bbb2313fc15bad8cfa7f1d4f3a1a7c
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-03-31 06:03:43 +0200

    Variables

--- Ex1_csharp-variables/varaibles.cs
+++ Ex1_csharp-variables/varaibles.cs
@@ -11,11 +11,21 @@ namespace CSharp.b.Variables
         static void Main(string[] args)
         {
             //define varaibles
+            // we have integer             
             var exit = 0;
+            // we have string             
             var str_n = "Jhon";
+            // we have real numbers
             var fixed_point = 5.5;
+            // we have char
             var symbol = 'A';
-            Console.WriteLine(exit);
-        }
+            // we have boolean
+            var bools = true;
+            Console.WriteLine("he is out of {0}",exit);
+            Console.WriteLine("his name is {1}",str_n);
+            Console.WriteLine("his rate is {2}",fixed_point);
+            Console.WriteLine("married: {4}",bools);
+            Console.WriteLine("tournment : {0}",symbols);  
+           }
     }
 }
