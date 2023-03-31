d7d25fe084cbf3ed2e59d60935910eabc3c66331
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-03-31 05:54:43 +0200

    Update and rename Test.cs to HelloWorld.cs

---
+++ Ex0_csharp-helloworld/HelloWorld.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+            /* hello world in C# */
+            var cs = "hello C#!";
+           // print it
+            Console.WriteLn(cs)
+
+
+        }
+    }
+}
